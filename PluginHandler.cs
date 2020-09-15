d27d1584141180c5d3cbd236e2a612331b276a26
Crawcik <crawcik@gmail.com>
2020-09-15 19:26:40 +0200

    Better handling for 3.9.0 version

--- PluginHandler.cs
+++ PluginHandler.cs
@@ -1,8 +1,11 @@
 ﻿using EventManager.Events;
+
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+
 using Smod2;
 using Smod2.Attributes;
+
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -14,13 +17,13 @@ namespace EventManager
     author = "Crawcik",
     configPrefix = "event",
     description = "Plugin with events",
-    id = "event.manager",
+    id = "crawcik.event_manager",
     langFile = "event",
     name = "Events Pack",
     SmodMajor = 3,
-    SmodMinor = 8,
-    SmodRevision = 4,
-    version = "3.0")]
+    SmodMinor = 9,
+    SmodRevision = 0,
+    version = "3.1")]
     public class PluginHandler : Plugin
     {
         public static PluginHandler Shared { private set; get; }
@@ -31,44 +34,54 @@ namespace EventManager
             {
                 string codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
                 System.UriBuilder uri = new System.UriBuilder(codeBase);
-                return Path.GetDirectoryName(System.Uri.UnescapeDataString(uri.Path)) + $"{Path.DirectorySeparatorChar}{this.Details.name}";
+                return Path.GetDirectoryName(System.Uri.UnescapeDataString(uri.Path)) + Path.DirectorySeparatorChar + this.Details.name;
             }
         }
 
         private CommandHandler commands;
-        public Dictionary<string, IDictionary<string, string>> AllTranslations;
-        private Dictionary<string, IDictionary<string, string>> DefaultTranslations = new Dictionary<string, IDictionary<string, string>>{
-                {
-                    "Versus", new Dictionary<string, string> {
-                        { "game_tutorial", "Event D-Class v.s. Scientists || Everyone has a gun || Checkpoints are locked" }
-                    }
-                }, {
-                    "Dziady", new Dictionary<string, string> {
-                        { "start", "Event 'Dziady' starts" },
-                        { "scp049_start", "You're a priest" },
-                        { "zombie_spawn", "The Dead rose from their graves!" }
-                    }
-                }, {
-                    "Trouble in Terrorist Town", new Dictionary<string, string> {
-                        { "not_enought_players", "Not enought players to start gamemode" },
-                        { "i_won", "Innocents won!" },
-                        { "t_won", "Traitors won!" },
-                        { "role_text", "Your role: " },
-                        { "d_tutorial", "Your task is to find all traitors. You can give orders to the innocents and use disarmer, to check if someone is traitor (you have one use)" },
-                        { "t_tutorial", "Your task is to kill everyone but other terrorists (cooperate with them). You can use the store, pick up a coin to open it" },
-                        { "i_tutorial", "Your task is survival. Follow the detective's orders" },
-                        { "ability_text1", "You can open ALL DOORS for 20 seconds!" },
-                        { "opened_menu", "You have an open store" },
-                        { "other_t", "Other rerrorists " },
-                        { "checker_positive", " is traitor!" },
-                        { "checker_negative", " is innocent" }
-                    }
-                }, {
-                    "Saxton Hale", new Dictionary<string, string> {
-                        { "tutorial", "<size=20>Janitor card - RAGE, MTF card - Taunt, O5 card - SPECJAL</size>" }
-                    }
+        public IDictionary<string, IDictionary<string, string>> AllTranslations;
+        private readonly static IDictionary<string, IDictionary<string, string>> DefaultTranslations = new Dictionary<string, IDictionary<string, string>>{
+            {
+                "Versus", new Dictionary<string, string> {
+                    { "game_tutorial", "Event D-Class v.s. Scientists || Everyone has a gun || Checkpoints are locked" }
                 }
-            };
+            },
+            {
+                "Dziady", new Dictionary<string, string> {
+                    { "start", "Event 'Dziady' starts" },
+                    { "scp049_start", "You're a priest" },
+                    { "zombie_spawn", "The Dead rose from their graves!" }
+                }
+            },
+            {
+                "Trouble in Terrorist Town", new Dictionary<string, string> {
+                    { "not_enought_players", "Not enought players to start gamemode" },
+                    { "i_won", "Innocents won!" },
+                    { "t_won", "Traitors won!" },
+                    { "role_text", "Your role: " },
+                    { "d_tutorial", "Your task is to find all traitors. You can give orders to the innocents and use disarmer, to check if someone is traitor (you have one use)" },
+                    { "t_tutorial", "Your task is to kill everyone but other terrorists (cooperate with them). You can use the store, pick up a coin to open it" },
+                    { "i_tutorial", "Your task is survival. Follow the detective's orders" },
+                    { "ability_text1", "You can open ALL DOORS for 20 seconds!" },
+                    { "opened_menu", "You have an open store" },
+                    { "other_t", "Other rerrorists " },
+                    { "checker_positive", " is traitor!" },
+                    { "checker_negative", " is innocent" }
+                }
+            },
+            {
+                "Saxton Hale", new Dictionary<string, string> {
+                    { "tutorial", "<size=20>Janitor card - RAGE, MTF card - Taunt, O5 card - SPECJAL</size>" },
+                    { "hale_spawn", "%nick% become %class%" }
+                }
+            },
+            {
+                "Prop Hunt", new Dictionary<string, string> {
+                    { "hunters_wait", "You will soon become hunter, find them all before decontamination!" },
+                    { "props_start", "You're now prop, try to hide somewhere before hunters find you!" }
+                }
+            }
+        };
         public override void OnDisable()
         {
             this.EventManager.RemoveEventHandlers(this);
@@ -77,37 +90,7 @@ namespace EventManager
 
         public override void OnEnable()
         {
-            if (this.config.Count == 0)
-            {
-                Info("Config has been generated");
-                AllTranslations = GenerateConfig();
-            }
-            else
-            {
-                try
-                {
-                    AllTranslations = config.ToObject<Dictionary<string,IDictionary<string,string>>>();
-                    Info("Config loaded");
-                }
-                catch
-                {
-                    Error("Config is incorrect!");
-                    this.PluginManager.DisablePlugin(this);
-                    return;
-                }
-            }
-            if (!IsConfigCorrect())
-                Warn("Config is incorrect! Please check if your config has any mistakes or isn't outdated! ");
-            commands.RegisterCommand(new Versus());
-            commands.RegisterCommand(new Dziady());
-            commands.RegisterCommand(new TTT());
-            commands.RegisterCommand(new Saxton_Hale());
-            //commands.RegisterCommand(new AlienBreakout());
-            commands.RegisterCommand(new PropHunt());
-        }
-
-        public override void Register()
-        {
+            this.Info(PluginDirectory + Path.DirectorySeparatorChar + "translation.json");
             if (!Directory.Exists(PluginDirectory))
             {
                 Directory.CreateDirectory(PluginDirectory);
@@ -115,14 +98,42 @@ namespace EventManager
             if (!File.Exists(PluginDirectory + Path.DirectorySeparatorChar + "translation.json"))
             {
                 File.Create(PluginDirectory + Path.DirectorySeparatorChar + "translation.json");
-                File.WriteAllText(PluginDirectory + Path.DirectorySeparatorChar + "translation.json", JsonConvert.SerializeObject(DefaultTranslations), Encoding.UTF8);
-                config = JObject.FromObject(DefaultTranslations);
+                this.AllTranslations = GenerateConfig();
+                this.Info("Created config in: " + PluginDirectory + Path.DirectorySeparatorChar + "translation.json");
             }
             else
             {
                 string json = File.ReadAllText(PluginDirectory + Path.DirectorySeparatorChar + "translation.json", Encoding.UTF8);
-                config = JObject.Parse(json);
+                if (string.IsNullOrEmpty(json))
+                {
+                    this.Error("Config file is empty!");
+                    this.AllTranslations = GenerateConfig();
+                }
+                else
+                {
+                    try
+                    {
+                        this.AllTranslations = JsonConvert.DeserializeObject<IDictionary<string, IDictionary<string, string>>>(json);
+                    }
+                    catch
+                    {
+                        this.Error("Config is incorrect! Loading default config");
+                        this.AllTranslations = DefaultTranslations;
+                    }
+                } 
             }
+            if (!IsConfigCorrect())
+                Warn("Config is incorrect! Please check if your config has any mistakes or isn't outdated! ");
+            commands.RegisterCommand(new Versus());
+            commands.RegisterCommand(new Dziady());
+            commands.RegisterCommand(new TTT());
+            commands.RegisterCommand(new Saxton_Hale());
+            commands.RegisterCommand(new AlienBreakout());
+            commands.RegisterCommand(new PropHunt());
+        }
+
+        public override void Register()
+        {
             Shared = this;
             commands = new CommandHandler();
             AddEventHandlers(commands);
@@ -131,20 +142,22 @@ namespace EventManager
 
         private bool IsConfigCorrect()
         {
-            if (AllTranslations.Keys == DefaultTranslations.Keys)
+            if (AllTranslations.Keys != DefaultTranslations.Keys)
                 return false;
-            foreach (string key in DefaultTranslations.Keys)
+            foreach (string key in AllTranslations.Keys)
             {
-                foreach (KeyValuePair<string, string> pairs in AllTranslations[key])
+                if(!DefaultTranslations.ContainsKey(key))
+                    return false;
+                foreach (KeyValuePair<string, string> pairs in DefaultTranslations[key])
                 {
-                    if (!DefaultTranslations[key].ContainsKey(pairs.Key))
+                    if (!AllTranslations[key].ContainsKey(pairs.Key))
                         return false;
                 }
             }
             return true;
         }
 
-        private Dictionary<string, IDictionary<string, string>> GenerateConfig()
+        private IDictionary<string, IDictionary<string, string>> GenerateConfig()
         {
             //Saving config
             File.WriteAllText(this.PluginDirectory + Path.DirectorySeparatorChar + "translation.json", JsonConvert.SerializeObject(DefaultTranslations), Encoding.UTF8);
