b5f0afbb5286278649c9c25b61613d2ef6697a81
Crawcik <crawcik@gmail.com>
2020-09-20 09:04:14 +0200

    Translation repair

--- Events/Saxton Hale.cs
+++ Events/Saxton Hale.cs
@@ -68,9 +68,9 @@ namespace EventManager.Events
             {
                 ev.Server.Map.ClearBroadcasts();
                 string message = Translation["hale_spawn"];
-                message.Replace("%nick%", boss.player.Name);
-                message.Replace("%class%", boss.role.ToString());
-                message.Replace("%hp%", boss.player.HP.ToString());
+                message = message.Replace("%nick%", boss.player.Name);
+                message = message.Replace("%class%", boss.role.ToString());
+                message = message.Replace("%hp%", boss.player.HP.ToString());
                 ev.Server.Map.Broadcast(2, message, false);
             }
             else
